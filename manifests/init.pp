a2c56f6fdf08b676fa546f2cd7e913334157c236
Michael Williams <dinosaur@riseup.net>
2013-03-19 16:31:10 -0700

    added vundle and useraccount

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,85 +24,105 @@
 # Copyright 2013 Andrew Leonard
 #
 class homedir(
-  $user = 'michael'
+  $user = 'michael',
   $path = '/home/michael',
   ) {
+
+  users::useraccount { "${user}":
+    ensure => present,
+    fullname => "${user}",
+    groups => ["sudo"],
+    shell => '/bin/zsh',
+  }
 
   file { "${path}/.zshrc":
     mode   => '0400',
     source => 'puppet:///modules/homedir/zshrc',
-    owner => '${user}',
-    group => '${user}',
+    owner => "${user}",
+    group => "${user}",
   }
 
   file { "${path}/.zprofile":
     mode   => '0400',
     source => 'puppet:///modules/homedir/zprofile',
-    owner => '${user}',
-    group => '${user}',
+    owner => "${user}",
+    group => "${user}",
   }
   
   file { "${path}/.zlogin":
     mode   => '0400',
     source => 'puppet:///modules/homedir/zlogin',
-    owner => '${user}',
-    group => '${user}',
+    owner => "${user}",
+    group => "${user}",
   }
 
   file { "${path}/.zshenv":
     mode   => '0400',
     source => 'puppet:///modules/homedir/zshenv',
-    owner => '${user}',
-    group => '${user}',
+    owner => "${user}",
+    group => "${user}",
   }
 
   file { "${path}/.xsession":
     mode   => '0400',
     source => 'puppet:///modules/homedir/xsession',
-    owner => '${user}',
-    group => '${user}',
+    owner => "${user}",
+    group => "${user}",
   }
 
   file { "${path}/.xinitrc":
     mode   => '0400',
     source => 'puppet:///modules/homedir/xinitrc',
-    owner => '${user}',
-    group => '${user}',
+    owner => "${user}",
+    group => "${user}",
   }
 
   file { "${path}/.vimrc":
     mode   => '0400',
     source => 'puppet:///modules/homedir/vimrc',
-    owner => '${user}',
-    group => '${user}',
+    owner => "${user}",
+    group => "${user}",
   }
 
   file { "${path}/.tmux.conf":
     mode   => '0400',
     source => 'puppet:///modules/homedir/tmux.conf',
-    owner => '${user}',
-    group => '${user}',
+    owner => "${user}",
+    group => "${user}",
   }
 
   file { "${path}/.gitconfig":
     mode   => '0400',
     source => 'puppet:///modules/homedir/gitconfig',
-    owner => '${user}',
-    group => '${user}',
+    owner => "${user}",
+    group => "${user}",
   }
 
   file { "${path}/.Xresources":
     mode   => '0400',
     source => 'puppet:///modules/homedir/Xresources',
-    owner => '${user}',
-    group => '${user}',
+    owner => "${user}",
+    group => "${user}",
   }
 
   vcsrepo { "${path}/.oh-my-zsh":
       ensure => 'present',
       provider => 'git',
       source => 'https://github.com/robbyrussell/oh-my-zsh',
-      user => '${user}',
-      group => '${user}',
+      user => "${user}",
+      group => "${user}",
+  }
+
+  file { "${path}/.vim/bundle":
+    ensure => directory,
+    mode => '0700',
+  }
+
+  vcsrepo { "${path}/.vim/bundle/vundle":
+      ensure => 'present',
+      provider => 'git',
+      source => 'https://github.com/gmarik/vundle',
+      user => "${user}",
+      group => "${user}",
   }
 }
